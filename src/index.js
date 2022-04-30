b86528b37c255cc6d4a8d4ae1f12a551b5273680
Nela Letochová <nela.letochova.wxc@gmail.com>
2022-04-30 10:39:13 +0200

    Zprovoznění navigace

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './style.css';
 
 console.log('funguju!');
+
+// (3) Zprovoznění navigace
+const navBtnElm = document.querySelector('#nav-btn');
+navBtnElm.addEventListener('click', () => {
+  document.querySelector('nav').classList.toggle('nav-closed');
+});
+
+const selectSection = () => {
+  document.querySelector('nav').classList.toggle('nav-closed');
+};
+const sections = document.querySelectorAll('.nav-closed a');
+for (let i = 0; i < sections.length; i++) {
+  sections[i].addEventListener('click', selectSection);
+}
