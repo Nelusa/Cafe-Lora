8347691b636121231b86620543d979ad540d1541
Nela Letochová <nela.letochova.wxc@gmail.com>
2022-04-30 12:17:28 +0200

    Ingredience jako komponenty - tvorba komponenty Layer a úprava všech dokumentů

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css';
+import './Layer/style.css';
 
 console.log('funguju!');
 
@@ -19,7 +20,7 @@ for (let i = 0; i < sections.length; i++) {
 // (4) Objednávání
 
 const orderBtnElm = document.querySelector('.order-btn');
-const ordered = () => {
+const orderCoffee = () => {
   if (orderBtnElm.textContent === 'Objednat') {
     orderBtnElm.textContent = 'Zrušit';
   } else if (orderBtnElm.textContent === 'Zrušit') {
@@ -30,18 +31,59 @@ const ordered = () => {
     .classList.toggle('drink__cup--selected');
 };
 
-orderBtnElm.addEventListener('click', ordered);
+orderBtnElm.addEventListener('click', orderCoffee);
 
-/*const unordered = () => {
-  orderBtnElm.textContent = 'Objednat';
-  document
-    .querySelector('.drink__cup')
-    .classList.remove('drink__cup--selected');
+// (5) Komponenty
+
+// (a) základní verze
+/*const layer1 = {
+  color: '#feeeca',
+  label: 'mléčná pěna',
+};
+const layer2 = {
+  color: '#fed7b0',
+  label: 'teplé mléko',
+};
+const layer3 = {
+  color: '#613916',
+  label: 'espresso',
 };
 
-if (ordered === true) {
-  orderBtnElm.addEventListener('click', unordered);
+const Layer = (props) => {
+  const { color, label } = props;
+
+  return `
+<div class="layer">
+<div class="layer__color" style="background-color: ${color}"></div>
+<div class="layer__label">${label}</div>
+</div>
+`;
+}; 
+
+const layerElm = document.querySelector('.drink__info');
+layerElm.innerHTML += Layer(layer1);
+layerElm.innerHTML += Layer(layer2);
+layerElm.innerHTML += Layer(layer3); */
+
+// (b) druhá fáze
+import { Layer } from './Layer/index.js';
+
+const layers = [
+  {
+    color: '#feeeca',
+    label: 'mléčná pěna',
+  },
+  {
+    color: '#fed7b0',
+    label: 'teplé mléko',
+  },
+  {
+    color: '#613916',
+    label: 'espresso',
+  },
+];
+
+const layerElm = document.querySelector('.drink__info');
+for (let i = 0; i < layers.length; i++) {
+  layerElm.innerHTML += Layer(layers[i]);
 }
-if (ordered === false) {
-  orderBtnElm.addEventListener('click', ordered);
-} */
