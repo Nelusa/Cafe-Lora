2cd9d82825f2d490aaf5006a6833ab3fd60e42f3
Nela Letochová <nela.letochova.wxc@gmail.com>
2022-04-30 11:41:13 +0200

    Objednávání - funkcionalita

--- src/index.js
+++ src/index.js
@@ -15,3 +15,33 @@ const sections = document.querySelectorAll('.nav-closed a');
 for (let i = 0; i < sections.length; i++) {
   sections[i].addEventListener('click', selectSection);
 }
+
+// (4) Objednávání
+
+const orderBtnElm = document.querySelector('.order-btn');
+const ordered = () => {
+  if (orderBtnElm.textContent === 'Objednat') {
+    orderBtnElm.textContent = 'Zrušit';
+  } else if (orderBtnElm.textContent === 'Zrušit') {
+    orderBtnElm.textContent = 'Objednat';
+  }
+  document
+    .querySelector('.drink__cup')
+    .classList.toggle('drink__cup--selected');
+};
+
+orderBtnElm.addEventListener('click', ordered);
+
+/*const unordered = () => {
+  orderBtnElm.textContent = 'Objednat';
+  document
+    .querySelector('.drink__cup')
+    .classList.remove('drink__cup--selected');
+};
+
+if (ordered === true) {
+  orderBtnElm.addEventListener('click', unordered);
+}
+if (ordered === false) {
+  orderBtnElm.addEventListener('click', ordered);
+} */
